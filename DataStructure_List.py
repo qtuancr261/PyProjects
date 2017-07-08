fbe05d3be1bfa8a0b7cd7aeb619c9a5d71be471f
thieuquangtuan <tuancr261@gmail.com>
2017-07-08 17:38:54 +0700

    Test built-in data structure : list in Python

---
+++ DataStructure_List.py
@@ -0,0 +1,37 @@
+emptyList = []
+print("An empty list in Python: ")
+for obj in emptyList:
+    print(obj)
+
+print("A normal list in Python: ")
+normalList = [15, 200.2, -320, 400]
+for obj in normalList:
+    print(obj)
+
+print("A string list in Python: ")
+stringList = ["GTX 460", "GTX 560", "GTX 660", "GTX 760", "GTX 960", "GTX 1060"]
+for obj in stringList:
+    print(obj)
+
+print("A mixed-type list in Python: ")
+mixedList = ["08/07/2017", "Nvidia", 220, 15.06, "ThieuQuangTuan"]
+for obj in mixedList:
+    print(obj)
+
+print("Lists inside of a list")
+lists = [[15, 22, 2017], ["RX460", "RX470"], [15.2, "AMD", 2016]]
+for ls in lists:
+    for obj in ls:
+        print(obj)
+
+vowels = ['u', 'e', 'o', 'a', 'i']
+checkWord = input("Give me a word to search fo vowels: ")
+checkWord += " GT "
+foundChar = []
+for char in checkWord:
+    if char in vowels and char not in foundChar:
+        foundChar.append(char)
+for char in foundChar:
+    print(char, checkWord.capitalize())
+
+
