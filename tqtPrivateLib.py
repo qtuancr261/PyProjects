d7a9d867b364d325b8cd8f0d6c36e0c293fc56ed
thieuquangtuan <tuancr261@gmail.com>
2017-08-05 21:50:19 +0700

    test python3 sdist

---
+++ tqtPrivateLib.py
@@ -0,0 +1,13 @@
+def check_prime(numberPara:int = 2) -> bool:
+    """With argument"""
+    if numberPara < 2:
+        return False
+    for checkValue in range(2, int(numberPara / 2) + 1, 1):
+        if numberPara % checkValue == 0:
+            return False
+    return True
+
+
+def searchLetters(phrase: str, letters: str = 'aeiou') -> set:
+    """Return a set of the letters found in phrase"""
+    return set(letters).intersection(set(phrase))
