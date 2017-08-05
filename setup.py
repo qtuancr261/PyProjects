b5fa6c37db11e963fba2adb651abc4ba0fc036bc
thieuquangtuan <tuancr261@gmail.com>
2017-08-05 21:47:39 +0700

    import setuptools module - setup()

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='tqtPrivateLib',
+    version='1.0-beta.1',
+    description='My first functions lib',
+    author='Rear Admiral TQT',
+    author_email='tuancr261@gmail.com',
+    url='headfirstlabs.com',
+    py_modules=['tqtPrivateLib']
+)
