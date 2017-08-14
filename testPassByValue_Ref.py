9cb69e8b4f9f63067486a61180d204dc0534577d
thieuquangtuan <tuancr261@gmail.com>
2017-08-14 21:53:18 +0700

    Update Project 21:53 14/08/2017 thieuquangtuan

--- testPassByValue_Ref.py
+++ testPassByValue_Ref.py
@@ -12,6 +12,13 @@ def pass_by_list(arg: list):
     arg.append('Crush cua Thieu la ai ?')
     print('After: ', arg)
 
+
+def pass_by(arg):
+    print('Before: ', arg)
+    arg = arg * 10
+    print('After: ', arg)
+
+
 print(sys.platform)
 print('Let demonstrate Call-By-Value')
 value = 100
@@ -20,3 +27,12 @@ print('Let see the value after passing it to pass_by_int(int) function: ', value
 text = list('Cau hoi kho trong ngay -> ')
 pass_by_list(text)
 print('Let see the text after passing it to pass_by_str(str) function: ', text)
+#-------------------------------------------------
+print('After that, python will show us a magic: ')
+pass_by(value)
+print('Outside pass_by(value): value = ', value)
+#---------------------------------------------------
+listNums = [15, 20, 25, 30, 35, 40]
+pass_by(listNums)
+print('Outside pass_by(listNums): listNums = ', listNums)
+print('Type: ', type(listNums))
